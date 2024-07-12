b03ae0410e301f312c8a135d1d614a317b42ea15
Jonathan <jon.chua51@gmail.com>
2024-07-12 16:10:21 -0700

    Script requires 3 values to run which will determine the proper file to get from go.dev to install

--- updateGo.sh
+++ updateGo.sh
@@ -2,18 +2,50 @@
 
 # Function to print usage
 usage() {
-  echo "Usage: $0 <version>"
-  echo "Example: $0 1.22.5"
+  echo "Usage: $0 <OS> <architecture> <version>"
+  echo "Example: $0 linux amd 1.22.5"
   exit 1
 }
 
-# Check if version number is provided
-if [ -z "$1" ]; then
+# Check if all arguments are provided
+if [ $# -ne 3 ]; then
   usage
 fi
 
-VERSION=$1
-URL="https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz"
+# Assign input to variables and convert to lowercase
+OS=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+ARCH=$(echo "$2" | tr '[:upper:]' '[:lower:]')
+VERSION=$3
+
+# Normalize OS
+case $OS in
+  linux|ubuntu|fedora)
+    OS="linux"
+    ;;
+  mac|macos)
+    OS="darwin"
+    ;;
+  *)
+    echo "Unsupported OS: $OS"
+    usage
+    ;;
+esac
+
+# Normalize ARCH
+case $ARCH in
+  arm|arm64)
+    ARCH="arm64"
+    ;;
+  amd|amd64|intel|x86|x64)
+    ARCH="amd64"
+    ;;
+  *)
+    echo "Unsupported architecture: $ARCH"
+    usage
+    ;;
+esac
+
+URL="https://go.dev/dl/go${VERSION}.${OS}-${ARCH}.tar.gz"
 
 # Step 1: Remove the current version of Go
 echo "Removing current Go installation..."
@@ -21,11 +53,11 @@ sudo rm -rf /usr/local/go
 
 # Step 2: Download the specified version of Go
 echo "Downloading Go version ${VERSION} from ${URL}..."
-wget $URL -O /tmp/go${VERSION}.linux-amd64.tar.gz
+wget $URL -O /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
 
 # Step 3: Extract the downloaded tarball to /usr/local
 echo "Extracting Go ${VERSION} to /usr/local..."
-sudo tar -C /usr/local -xzf /tmp/go${VERSION}.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf /tmp/go${VERSION}.${OS}-${ARCH}.tar.gz
 
 # Step 4: Update PATH environment variable
 echo "Updating PATH to include /usr/local/go/bin..."
